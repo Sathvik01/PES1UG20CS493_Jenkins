500172d9e2453d3dc094d816eac340698b7681ec
Sathvik <svv.sathvik@gmail.com>
2023-02-17 11:49:02 +0530

    hello.cpp changed

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "PES1UG20CS493";
+  cout << "PES1UG20CS493_Sathvik";
   return 0;
 }
